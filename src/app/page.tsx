b754c7c6e0e1ad6f6ba701a85afd0951efee44a2
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2026-02-12 12:19:13 -0600

    feat: set up initial Next.js application structure with home page, global styles, and product seed data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function HomePage() {
   return (
     <section className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
+      <h1 className="fade-in">Home Page</h1>
     </section>
   );
 }

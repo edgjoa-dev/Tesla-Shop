75b7b014f158f9e41ef345b3bf85a6e143d76f6a
Edgar Joaquin <floresedgarjoaquin@gmail.com>
2026-02-16 19:53:11 -0600

    feature/HU191-CreaciónRutasdeApp - Creación de rutas de aplicación para todo el ecommerce

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderPage() {
+    return (
+        <section>
+            <h1>Order: #21sf1sd31fsf</h1>
+        </section>
+    );
+}
---
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function ProductPage() {
+    return (
+        <section>
+            <h1>Product Nombre_del_Producto_slug</h1>
+        </section>
+    );
+}

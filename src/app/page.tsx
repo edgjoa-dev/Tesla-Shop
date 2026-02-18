ea195bfab8464a6f062f2466b917233cc3d9bf1a
Edgar Joaquin <floresedgarjoaquin@gmail.com>
2026-02-17 18:17:42 -0600

    feature/HU192-TopMenu : Se implementa navbar con links de navegación a las diferentes secciones de ecommerce con iconos y responsive

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { fontTitle } from "@/config/fonts";
-
-export default function HomePage() {
-  return (
-    <section className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className={`${fontTitle.className} fade-in text-3xl font-bold`}>Home Page</h1>
-    </section>
-  );
-}

e9527cbe130d4b09093cab7497283c782e1deb41
Edgar Joaquin <floresedgarjoaquin@gmail.com>
2026-04-10 10:19:09 -0600

    E-218-fix-correccion-slide-show: Se realiza la corrección al importar paquetes de Swiper

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -21,29 +21,30 @@ export default async function ProductPage({ params }: Props) {
     }
 
     return (
-        <section className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3 h-svh">
+        <section className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-4 gap-8">
             {/* Slideshow */}
-            <div className=" col-span-1 md:col-span-2 h-[700]">
-                <SlideShow images={product.images} title={product.title} className="h-full bg-teal-500" />
+            <div className="col-span-1 md:col-span-2 px-5">
+                <SlideShow images={product.images} title={product.title} />
             </div>
 
             {/* Detalles */}
-            <div className=" col-span-2 md:col-span-1 ">
-                <h1 className={`${fontTitle.className} antialiased font-bold text-2xl mb-2`}> {product.title} </h1>
-                <p className="text-lg mb-5 font-black"> $ {product.price} </p>
-                <span className="font-bold text-md">
-                    Desripción
-                </span>
-                <p className="font-medium text-sm">{product.description}</p>
+            <div className="col-span-1 md:col-span-2 px-5">
+                <h1 className={` ${fontTitle.className} antialiased font-bold text-xl`}> {product.title} </h1>
+                <p className="text-lg mb-5">${product.price}</p>
+
+                <h3 className="font-bold text-sm">Descripción</h3>
+                <p className="font-light">
+                    {product.description}
+                </p>
 
                 {/* Selectores */}
-                <SelectedSize selectedSize={ product.sizes[0] } availableSizes={product.sizes} />
+                <SelectedSize selectedSize={product.sizes[0]} availableSizes={product.sizes} />
 
                 <QuantitySelctor />
 
-                <div className="flex flex-col gap-2 w-fit">
-                    <button className="btn-primary mt-4 rounded-md"> Agregar al Carito </button>
-                    <button className="bg-gray-950 text-white py-2 px-3 rounded-sm"> Agregar a Favoritos </button>
+                <div className="flex flex-col justify-end items-center gap-2 py-8 w-2/4">
+                    <button className="btn-primary mt-4 rounded-md w-full"> Agregar al Carito </button>
+                    <button className="bg-gray-950 text-white py-2 px-3 rounded-sm w-full"> Agregar a Favoritos </button>
                 </div>
             </div>
         </section>
